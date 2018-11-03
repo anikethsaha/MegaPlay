5ccbde85ea4d4f194a9b225b31eb26c6eaaf2553
ABIN BENNY <666.abin007@gmail.com>
2018-11-03 15:17:38 +0530

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,15 @@
-// Get the modal
-var modal = document.getElementById('Canavas1');
-var modal1 = document.getElementById('Canavas2');
 
-
-// Get the button that opens the modal
-var btn = document.getElementById("front");
-var btn1 = document.getElementById("back");
-btn.disabled = true;
-
-btn.onclick = function() {
-    modal.style.display = "block";
-    modal1.style.display = "none";
-    btn.disabled=true;
-    btn1.disabled = false;
+$(document).ready(function){
+    $(".menu-icon").on("click",function(){
+        $("nav ul").toggleClass("showing");
+    });
+});
+// Scrolling Effect
+$(window).on("scroll",Function(){
+             if($window.scrollTop()){
+    $('nav').addClass('black');
 }
-btn1.onclick = function() {
-    modal.style.display = "none";
-    modal1.style.display = "block";
-    btn.disabled = false;
-    btn1.disabled = true;
+else{
+    $('nav').removeClass('black');
 }
+             })
