b69fbf5895cb29f2918ec2396223ec947f999e0b
anikethsaha <anik220798@gmail.com>
2018-11-04 23:24:45 +0530

    Added the fullPageSLider and Changed the Header

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 
-$(document).ready(function){
+$(document).ready(function(){
     $(".menu-icon").on("click",function(){
         $("nav ul").toggleClass("showing");
     });
 });
 // Scrolling Effect
-$(window).on("scroll",Function(){
-             if($window.scrollTop()){
+$(window).on("scroll",function(){
+
+    if($(window).scrollTop()){
     $('nav').addClass('black');
-}
-else{
+    }
+    else{
     $('nav').removeClass('black');
-}
-             })
+    }
+})
