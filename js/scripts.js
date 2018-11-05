46c834f1acef83c87af87e9354a4a8df681ad8da
anikethsaha <anik220798@gmail.com>
2018-11-05 13:05:07 +0530

    Added The service wroker and browser Caching

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,90 @@
 
-$(document).ready(function){
+$(document).ready(function(){
     $(".menu-icon").on("click",function(){
         $("nav ul").toggleClass("showing");
     });
 });
 // Scrolling Effect
-$(window).on("scroll",Function(){
-             if($window.scrollTop()){
+$(window).on("scroll",function(){
+
+    if($(window).scrollTop()){
     $('nav').addClass('black');
-}
-else{
+    }
+    else{
     $('nav').removeClass('black');
-}
-             })
+    }
+})
+        new fullpage('#fullpage', {
+            //options here
+            autoScrolling:true,
+            scrollHorizontally: true,
+            sectionSelector: '.section',
+            slideSelector: '.slide',
+            navigation: true,
+            navigationPosition: 'right',
+            showActiveTooltip: false,
+            slidesNavigation: false,
+            slidesNavPosition: 'bottom',
+            lazyLoading: true,
+            css3: true,
+            scrollingSpeed: 700,
+            autoScrolling: true,
+            fitToSection: true,
+            fitToSectionDelay: 1000,
+            scrollBar: false,
+            easing: 'easeInOutCubic',
+            easingcss3: 'ease',
+            loopBottom: true,
+            loopTop: false,
+            loopHorizontal: true,
+            continuousVertical: false,
+            continuousHorizontal: false,
+            scrollHorizontally: false,
+            interlockedSlides: false,
+            dragAndMove: false,
+            offsetSections: false,
+            resetSliders: false,
+            fadingEffect: false,
+            sectionsColor : ['white', 'white','red', 'yellow'],
+            scrollOverflow: false,
+            scrollOverflowReset: false,
+            scrollOverflowOptions: null,
+            touchSensitivity: 15,
+            normalScrollElementTouchThreshold: 5,
+            bigSectionsDestination: null,
+            controlArrows: true,
+            verticalCentered: true,
+            paddingTop: '3em',
+            paddingBottom: '10px',
+            fixedElements: '#header, #footer',
+            responsiveWidth: 0,
+            responsiveHeight: 0,
+            responsiveSlides: true,
+            parallax: true,
+            parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+        })
+        //methods
+        fullpage_api.setAllowScrolling(false);
+
+
+      // Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+
+
---
+++ sw.js
@@ -0,0 +1,55 @@
+var CACHE_NAME = 'MEGAPLAY_v2';
+
+self.addEventListener('install',e => {
+
+})
+
+self.addEventListener('activate',e => {
+
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cache => {
+              if (cache !== CACHE_NAME) {
+                console.log(' Clearing Old Cache');
+                return caches.delete(cache);
+              }
+            })
+          );
+        })
+      );
+})
+
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
